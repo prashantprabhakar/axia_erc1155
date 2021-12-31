8216adb275f39e8b385e5de453eebfc4da9757d3
prashant.prabhakar <prashant.prabhakar@sofocle.com>
2021-12-31 12:40:22 +0530

    fix commemts

--- smart_contracts/BaseERC1155.sol
+++ smart_contracts/BaseERC1155.sol
@@ -6,83 +6,110 @@ import "./ERC1155.sol";
 import "./Freezable.sol";
 
 contract BaseERC1155 is ERC1155, Freezable {
-  
-  constructor(string memory uri) ERC1155(uri) {
-  }
 
-  function directMint(
-    address to,
-    uint256 id,
-    uint256 amount,
-    bytes memory data,
-    string memory _uri
-  )
-    public
-    onlyAdmin
-    isUnfreezed(to)
-  {
-    _mint(to, id, amount, data, _uri);
-  }
+    constructor(string memory uri) ERC1155(uri) {}
 
-  function directMintBatch(
-    address to,
-    uint256[] memory ids,
-    uint256[] memory amounts,
-    bytes memory data,
-    string[] memory _uris
-  ) public onlyAdmin isUnfreezed(to) {
-    _mintBatch(to, ids, amounts, data, _uris);
-  }
+    /**
+     * @dev See {ERC1155-safeTransferFrom}.
+     *
+     * Requirements:
+     *
+     * - `from` must be unfreezed
+     * - `to` must be unfreezed
+     * -  Contract should not be under emergency freeze
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    )
+        public
+        virtual
+        override
+        isUnfreezed(from)
+        isUnfreezed(to)
+        noEmergencyFreeze
+    {
+        super.safeTransferFrom(from, to, id, amount, data);
+    }
 
-  function safeTransferFrom(
-    address from,
-    address to,
-    uint256 id,
-    uint256 amount,
-    bytes memory data
-  ) isUnfreezed(from) isUnfreezed(to) public virtual override {
-    super.safeTransferFrom(from, to, id, amount, data);
-  }
-
-  function safeBatchTransferFrom(
+    /**
+     * @dev See {ERC1155-safeBatchTransferFrom}.
+     *
+     * Requirements:
+     *
+     * - `from` must be unfreezed
+     * - `to` must be unfreezed
+     * -  Contract should not be under emergency freeze
+     */
+    function safeBatchTransferFrom(
         address from,
         address to,
         uint256[] memory ids,
         uint256[] memory amounts,
         bytes memory data
-    ) isUnfreezed(from) isUnfreezed(to) public virtual override {
-      super.safeBatchTransferFrom(from, to, ids, amounts, data);
+    )
+        public
+        virtual
+        override
+        isUnfreezed(from)
+        isUnfreezed(to)
+        noEmergencyFreeze
+    {
+        super.safeBatchTransferFrom(from, to, ids, amounts, data);
     }
 
-  function setTokenURIPrefix(string memory tokenURIPrefix) public onlyOwner {
-    _setTokenURIPrefix(tokenURIPrefix);
-  }
+    /**
+     * @dev See {HasTokenURI-_setTokenURIPrefix}.
+     *
+     * Requirements:
+     *
+     * - onlyOwner is allowed to call this function
+     */
+    function setTokenURIPrefix(string memory tokenURIPrefix) public onlyOwner {
+        _setTokenURIPrefix(tokenURIPrefix);
+    }
 
-  /**
-    * @dev Public function to set the token URI for a given token.
-    * Reverts if the token ID does not exist.
-    * @param tokenId uint256 ID of the token to set its URI
-    * @param uri string URI to assign
-    */
-  function setTokenURI(uint256 tokenId, string memory uri) public onlyAdmin shouldExist(tokenId) {
-    super._setTokenURI(tokenId, uri);
-  }
+    /**
+     * @dev Public function to set the token URI for a given token.
+     * Reverts if the token ID does not exist.
+     * @param tokenId uint256 ID of the token to set its URI
+     * @param uri string URI to assign
 
+     * Requirements:
+     *
+     * - onlyAdmin is allowed to call this function
+     * - tokenId shoudld already exist
+     */
+    function setTokenURI(uint256 tokenId, string memory uri)
+        public
+        onlyAdmin
+        shouldExist(tokenId)
+    {
+        super._setTokenURI(tokenId, uri);
+    }
 
-  function burn(
-    address from,
-    uint256 id,
-    uint256 amount
-  ) public {
-    _burn(from, id, amount);
-  }
+    /**
+     * @dev See {ERC1155-_burn}.
+     */
+    function burn(
+        address from,
+        uint256 id,
+        uint256 amount
+    ) public {
+        _burn(from, id, amount);
+    }
 
-  function burnBatch(
-    address from,
-    uint256[] memory ids,
-    uint256[] memory amounts
-  ) public {
-    _burnBatch(from, ids, amounts);
-  }
-
+    /**
+     * @dev See {ERC1155-_burnBatch}.
+     */
+    function burnBatch(
+        address from,
+        uint256[] memory ids,
+        uint256[] memory amounts
+    ) public {
+        _burnBatch(from, ids, amounts);
+    }
 }
--- smart_contracts/CustomERC1155.sol
+++ smart_contracts/CustomERC1155.sol
@@ -6,127 +6,221 @@ import "./BaseERC1155.sol";
 import "./SecondaryMarketFee.sol";
 
 contract CustomERC1155 is BaseERC1155, SecondaryMarketFee {
+    // A nonce value to prevent replay of same signed transaction.
+    mapping(address => mapping(uint256 => bool)) public isNonceUsed;
 
-  mapping(address => mapping(uint => bool)) public isNonceUsed;
-
-  struct Signature {
-    bytes32 r;
-    bytes32 s;
-    uint8 v;
-  }
-
-  constructor(string memory uri) BaseERC1155(uri) {
-    _registerInterface(_INTERFACE_ID_FEES);
-  }
-
-  function mint(
-    address to,
-    uint256 id,
-    uint256 amount,
-    bytes memory data,
-    string memory _uri,
-    Fee[] memory _fees,
-    Signature calldata adminSignature,
-    uint256 customNonce
-  ) public returns(bool) {
-
-    bytes32 signedMessage = keccak256(abi.encode(
-      address(this),
-      customNonce,
-      // add chainId for replay protection
-      to,
-      id,
-      amount,
-      data
-    ));
-    validateSigning(signedMessage, adminSignature, customNonce);
-    _mint(to, id, amount, data, _uri);
-    super.addFees(id, _fees);
-    return true;
-  }
-
-  function mintBatch(
-    address to,
-    uint256[] memory ids,
-    uint256[] memory amounts,
-    bytes memory data,
-    string[] memory _uris,
-    Fee[] memory fees,  // fee is same for all ids..
-    Signature calldata adminSignature,
-    uint256 customNonce
-  ) public returns (bool) {
-
-    bytes32 signedMessage = keccak256(abi.encode(
-      address(this),
-      customNonce,
-      // add chainId for replay protection
-      to,
-      ids,
-      amounts,
-      data,
-      fees
-    ));
-    validateSigning(signedMessage, adminSignature, customNonce);
-    _mintBatch(to, ids, amounts, data, _uris);
-    for (uint256 i = 0; i < ids.length; i++) {
-      super.addFees(ids[i], fees);
+    struct Signature {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
     }
-    return true;
 
-  }
+    constructor(string memory uri) BaseERC1155(uri) {
+        _registerInterface(_INTERFACE_ID_FEES);
+    }
 
-  function setTokenURI(uint256 tokenId, string memory uri, Signature calldata adminSignature, uint256 customNonce)
-    public
-    shouldExist(tokenId)
-  {
-   bytes32 signedMessage = keccak256(abi.encode(
-      address(this),
-      customNonce,
-      // add chainId for replay protection
-      tokenId,
-      uri
-    ));
-    validateSigning(signedMessage, adminSignature, customNonce);
-    super._setTokenURI(tokenId, uri);
-  }
+    /**
+     * @dev See {ERC1155-_mint}.
+     *
+     * Adds royality fee for secondary market sales to `id`
+     * Requirements:
+     *
+     * - Only Admin is allowed to call the function
+     * - `to` must be unfreezed
+     */
+    function directMint(
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data,
+        string memory _uri,
+        Fee[] memory _fees
+    ) public onlyAdmin isUnfreezed(to) {
+        _mint(to, id, amount, data, _uri);
+        super.addFees(id, _fees);
+    }
 
-  function signedSetApprovalForAll(
-    bool approved,
-    Signature calldata userSignature,
-    uint256 customNonce
-  ) public {
-    bytes32 signedMessage = keccak256(abi.encode(
-      address(this),
-      customNonce,
-      approved
-    ));
-    address signer = getSigner(signedMessage, userSignature);
-    require(!isFreezed(signer), "Signer is freezed");
-    require(!isNonceUsed[signer][customNonce], "Nonce is already used");
-    isNonceUsed[signer][customNonce] = true;
-    _setApprovalForAll(_msgSender(), signer, approved);
-  }
+    /**
+     * @dev See {ERC1155-_mintBatch}.
+     *
+     * Adds royality fee for secondary market sales to all `ids`
+     * Requirements:
+     *
+     * - Only Admin is allowed to call the function
+     * - `to` must be unfreezed
+     */
+    function directMintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data,
+        string[] memory _uris,
+        Fee[] memory _fees
+    ) public onlyAdmin isUnfreezed(to) {
+        _mintBatch(to, ids, amounts, data, _uris);
+        for (uint256 i = 0; i < ids.length; i++) {
+            super.addFees(ids[i], _fees);
+        }
+    }
 
+    /**
+     * @dev See {ERC11-_mint}
+     * Adds royality fee for secondary market sales to `id`
+     *
+     * Requirements:
+     *
+     * - Admin must sign the params of the function along with contract address, chainId and random nonce
+     */
+    function mint(
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data,
+        string memory _uri,
+        Fee[] memory _fees,
+        Signature calldata adminSignature,
+        uint256 customNonce
+    ) public isUnfreezed(to) returns (bool) {
+        bytes32 signedMessage = keccak256(
+            abi.encode(
+                address(this),
+                customNonce,
+                // add chainId for replay protection
+                to,
+                id,
+                amount,
+                data
+            )
+        );
+        validateSigning(signedMessage, adminSignature, customNonce);
+        _mint(to, id, amount, data, _uri);
+        super.addFees(id, _fees);
+        return true;
+    }
 
-  function kill(uint message) external onlyOwner {
-    require (message == 123456789987654321, "Invalid code");
-    // Transfer Eth to owner and terminate contract
-    selfdestruct(payable(msg.sender));
-  }
+    /**
+     * @dev See {ERC1155-_mintBatch}.
+     *
+     * Adds royality fee for secondary market sales to all `ids`
+     * Requirements:
+     *
+     * - Only Admin is allowed to call the function
+     * - `to` must be unfreezed
+     */
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data,
+        string[] memory _uris,
+        Fee[] memory fees, // fee is same for all ids..
+        Signature calldata adminSignature,
+        uint256 customNonce
+    ) public isUnfreezed(to) returns (bool) {
+        bytes32 signedMessage = keccak256(
+            abi.encode(
+                address(this),
+                customNonce,
+                // add chainId for replay protection
+                to,
+                ids,
+                amounts,
+                data,
+                fees
+            )
+        );
+        validateSigning(signedMessage, adminSignature, customNonce);
+        _mintBatch(to, ids, amounts, data, _uris);
+        for (uint256 i = 0; i < ids.length; i++) {
+            super.addFees(ids[i], fees);
+        }
+        return true;
+    }
 
+    /**
+     * @dev Public function to set the token URI for a given token.
+     *
+     * Requirements:
+     *
+     * - Admin must sign the params of the function along with contract address, chainId and random nonce
+     * - tokenId shoudld already exist
+     */
+    function setTokenURI(
+        uint256 tokenId,
+        string memory uri,
+        Signature calldata adminSignature,
+        uint256 customNonce
+    ) public shouldExist(tokenId) {
+        bytes32 signedMessage = keccak256(
+            abi.encode(
+                address(this),
+                customNonce,
+                // add chainId for replay protection
+                tokenId,
+                uri
+            )
+        );
+        validateSigning(signedMessage, adminSignature, customNonce);
+        super._setTokenURI(tokenId, uri);
+    }
 
-  function getSigner(bytes32 message, Signature calldata sig) public pure returns (address){
-    bytes memory prefix = "\x19Ethereum Signed Message:\n32";
-    return ecrecover(keccak256(abi.encodePacked(prefix, message)),sig.v, sig.r, sig.s);
-  }
+    /**
+     * @dev See {ERC1155-_setApprovalForAll}
+     *
+     * Requirements:
+     *
+     * - Admin must sign the params of the function along with contract address, chainId and random nonce
+     */
+    function signedSetApprovalForAll(
+        bool approved,
+        Signature calldata userSignature,
+        uint256 customNonce
+    ) public {
+        bytes32 signedMessage = keccak256(
+            abi.encode(address(this), customNonce, approved)
+        );
+        address signer = getSigner(signedMessage, userSignature);
+        require(!isFreezed(signer), "Signer is freezed");
+        require(!isNonceUsed[signer][customNonce], "Nonce is already used");
+        isNonceUsed[signer][customNonce] = true;
+        _setApprovalForAll(_msgSender(), signer, approved);
+    }
 
-  function validateSigning(bytes32 signedMessage, Signature calldata adminSignature, uint256 customNonce) private returns(address) {
-    address signer = getSigner(signedMessage, adminSignature);
-    require(!isFreezed(signer), "Signer is freezed");
-    require(isAdmin(signer), "admin signature is required");
-    require(!isNonceUsed[signer][customNonce], "Nonce is already used");
-    isNonceUsed[signer][customNonce] = true;
-    return signer;
-  }
+    /**
+     * @dev Allows owner to destroy the contract.
+     * MUST BE USED WITH UTMOST CARE.
+     */
+    function kill(uint256 message) external onlyOwner {
+        require(message == 123456789987654321, "Invalid code");
+        // Transfer Eth to owner and terminate contract
+        selfdestruct(payable(msg.sender));
+    }
 
+    function getSigner(bytes32 message, Signature calldata sig)
+        public
+        pure
+        returns (address)
+    {
+        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        return
+            ecrecover(
+                keccak256(abi.encodePacked(prefix, message)),
+                sig.v,
+                sig.r,
+                sig.s
+            );
+    }
+
+    function validateSigning(
+        bytes32 signedMessage,
+        Signature calldata adminSignature,
+        uint256 customNonce
+    ) private returns (address) {
+        address signer = getSigner(signedMessage, adminSignature);
+        require(!isFreezed(signer), "Signer is freezed");
+        require(isAdmin(signer), "admin signature is required");
+        require(!isNonceUsed[signer][customNonce], "Nonce is already used");
+        isNonceUsed[signer][customNonce] = true;
+        return signer;
+    }
 }
